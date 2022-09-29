03e307ec53d95170feb687893d8a3078ef1dce31
Rob Martin <rmartin3@alumni.unca.edu>
2022-09-29 13:32:50 -0400

    contact list and contact form styles, publications, useFind, methods

--- meteor-wallet/imports/ui/ContactForm.jsx
+++ meteor-wallet/imports/ui/ContactForm.jsx
@@ -1,10 +1,28 @@
-import React, { useRef } from "react";
-import { ContactsCollection } from "../api/ContactsCollection";
+import React, { useState, useRef } from "react";
+import { ErrorAlert } from "./components/ErrorAlert";
+import { SuccessAlert } from "./components/SuccessAlert";
+
+import { Meteor } from "meteor/meteor";
 
 export default function ContactForm() {
   const nameRef = useRef();
   const emailRef = useRef();
   const imageUrlRef = useRef();
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const showError = ({ message }) => {
+    setError(message);
+    setTimeout(() => {
+      setError("");
+    }, 3000);
+  };
+  const showSuccess = ({ message }) => {
+    setSuccess(message);
+    setTimeout(() => {
+      setSuccess("");
+    }, 3000);
+  };
 
   const handleSaveContact = () => {
     console.log(
@@ -19,27 +37,74 @@ export default function ContactForm() {
       imageUrl: imageUrlRef.current.value,
     };
 
-    ContactsCollection.insert(contact);
-    nameRef.current.value = "";
-    emailRef.current.value = "";
-    imageUrlRef.current.value = "";
+    Meteor.call("contacts.insert", contact, (errorResponse) => {
+      if (errorResponse) {
+        showError({ message: errorResponse.error });
+      } else {
+        nameRef.current.value = "";
+        emailRef.current.value = "";
+        imageUrlRef.current.value = "";
+        showSuccess({ message: "Contact saved." });
+      }
+    });
   };
+
   return (
-    <form action="">
-      <div>
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" ref={nameRef} />
+    <form className="mt-6">
+      {error && <ErrorAlert message={error} />}
+      {success && <SuccessAlert message={success} />}
+      <div className="grid grid-cols-6 gap-6">
+        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            ref={nameRef}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            ref={emailRef}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label
+            htmlFor="imageUrl"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Image URL
+          </label>
+          <input
+            type="text"
+            id="imageUrl"
+            ref={imageUrlRef}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
       </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="email" ref={emailRef} />
-      </div>
-      <div>
-        <label htmlFor="imageUrl">Image Url</label>
-        <input id="imageUrl" type="text" ref={imageUrlRef} />
-      </div>
-      <div>
-        <button type="button" onClick={handleSaveContact}>
+      <div className="px-2 py-3 text-right">
+        <button
+          type="button"
+          onClick={handleSaveContact}
+          className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+        >
           Save Contact
         </button>
       </div>
